d353a10cae8d56044abf28fc013edc79cc80648a
Muhammad Hassan Siddique <161647988+muhammadhassan0017@users.noreply.github.com>
2024-03-21 11:19:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+particlesJS.load('particles-js-1', './jsons/particles1.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+particlesJS.load('particles-js-2', './jsons/particles2.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+particlesJS.load('particles-js-3', './jsons/particles3.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+particlesJS.load('particles-js-4', './jsons/particles4.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+particlesJS.load('particles-js-5', './jsons/particles5.json', function () {
+    console.log('callback - particles.js config loaded');
+});
